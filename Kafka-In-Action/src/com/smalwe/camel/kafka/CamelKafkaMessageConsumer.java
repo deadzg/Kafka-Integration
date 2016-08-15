ce0b36f66d29c880e25248d310b57e3c1398d2c1
Sourabh <soumalwe@in.ibm.com>
2016-08-15 10:23:29 +0530

    Adding camel kafka message consumer

---
+++ Kafka-In-Action/src/com/smalwe/camel/kafka/CamelKafkaMessageConsumer.java
@@ -0,0 +1,23 @@
+package com.smalwe.camel.kafka;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelKafkaMessageConsumer implements Processor{
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		
+		//Thread.sleep(60000); This is for generating group dead and removed scenario
+		System.out.println("Inside the camelKafkaMessageConsumer");
+		System.out.println("Message:" + exchange.getIn().getBody().toString());
+		System.out.println("Partition Key:" + exchange.getIn().getHeader(KafkaConstants.PARTITION_KEY));
+		System.out.println("Partition:" + exchange.getIn().getHeader(KafkaConstants.PARTITION));
+		System.out.println("Topic:" + exchange.getIn().getHeader(KafkaConstants.TOPIC));
+		System.out.println("Offset:" + exchange.getIn().getHeader(KafkaConstants.OFFSET));
+	}
+	
+}
---
+++ Kafka-In-Action/src/com/smalwe/camel/kafka/SimpleCamelKafka.java
@@ -0,0 +1,21 @@
+package com.smalwe.camel.kafka;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class SimpleCamelKafka {
+	
+	public static void main(String[] args) throws Exception {
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("config/kafkaCamelContext.xml");
+		CamelContext camelContext = SpringCamelContext.springCamelContext(appContext, false);
+		for(String s : camelContext.getComponentNames()) {
+			System.out.println(s);
+		}
+		
+		System.out.println("Starting camel context..");
+		camelContext.start();
+		Thread.currentThread().join();
+	}
+}
